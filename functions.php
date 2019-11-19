23396b952c1c42431e69a918b1711644caf0a5e9
Chip Sheppard <chip@sheppco.com>
2019-11-18 19:39:26 -0500

    original commit

--- functions.php
+++ functions.php
@@ -16,7 +16,18 @@ define( 'OSIXTHREEO_CHILD_VERSION', '1.0.0' );
 /**
  * Enqueue styles
  */
-function child_enqueue_styles() {
-	wp_enqueue_style( 'osixthreeo-child-theme-css', get_stylesheet_directory_uri() . '/style.css', array( 'osixthreeo-style' ), OSIXTHREEO_CHILD_VERSION, 'all' );
+function osixthreeo_child_enqueue_styles() {
+	wp_deregister_style( 'osixthreeo-style' );
+	wp_enqueue_style( 'osixthreeo-parent-style', get_template_directory_uri() . '/style.css' );
+	wp_enqueue_style( 'osixthreeo-child-style', get_stylesheet_uri(), array( 'osixthreeo-parent-style' ) );
 }
-add_action( 'wp_enqueue_scripts', 'child_enqueue_styles', 15 );
+add_action( 'wp_enqueue_scripts', 'osixthreeo_child_enqueue_styles', 15 );
+
+/**
+ * Move the Custom Header.
+ */
+function osixthreeo_child_remove_customheader() {
+	 remove_action( 'osixthreeo_header_after_wrap', 'osixthreeo_display_customheader' );
+}
+add_action( 'init' , 'osixthreeo_child_remove_customheader' );
+add_action( 'tha_content_wrap_before', 'osixthreeo_display_customheader', 15 );
