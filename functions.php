546fd74d047c485e73bad02921f5586b265cdb26
Chip Sheppard <chip@sheppco.com>
2020-01-19 21:46:22 -0500

    remove test functions

--- functions.php
+++ functions.php
@@ -1,33 +1,42 @@
 <?php
 /**
  * OsixthreeO Child Theme functions and definitions
+ * Enqueues   parent and child stylesheets
  *
- * @link https://developer.wordpress.org/themes/basics/theme-functions/
- *
- * @package Osixthreeo Child
- * @since 1.0.0
+ * @package   Osixthreeo Child
+ * @since     1.0.0
  */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
 
 /**
  * Define Constants
  */
 define( 'OSIXTHREEO_CHILD_VERSION', '1.0.0' );
 
-/**
- * Enqueue styles
- */
-function osixthreeo_child_enqueue_styles() {
-	wp_deregister_style( 'osixthreeo-style' );
-	wp_enqueue_style( 'osixthreeo-parent-style', get_template_directory_uri() . '/style.css' );
-	wp_enqueue_style( 'osixthreeo-child-style', get_stylesheet_uri(), array( 'osixthreeo-parent-style' ) );
+if ( ! function_exists( 'osixthreeo_child_enqueue_styles' ) ) {
+	/**
+	 * Enqueue styles
+	 */
+	function osixthreeo_child_enqueue_styles() {
+
+		// Enqueue Parent theme's stylesheet.
+		wp_enqueue_style( 'osixthreeo-parent-style', get_template_directory_uri() . '/style.css' );
+
+		// Enqueue Child theme's stylesheet.
+		// Setting 'parent-style' as a dependency will ensure that the child theme stylesheet loads after it.
+		wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'osixthreeo-parent-style' ), OSIXTHREEO_CHILD_VERSION );
+
+	}
 }
 add_action( 'wp_enqueue_scripts', 'osixthreeo_child_enqueue_styles', 15 );
 
-/**
- * Move the Custom Header.
+
+/*
+ * -----------------------------------------------------------------
+ * CUSTOM CODE BEGINS HERE
+ * --------------------------------------------------------------
  */
-function osixthreeo_child_remove_customheader() {
-	 remove_action( 'osixthreeo_header_after_wrap', 'osixthreeo_display_customheader' );
-}
-add_action( 'init' , 'osixthreeo_child_remove_customheader' );
-add_action( 'tha_content_wrap_before', 'osixthreeo_display_customheader', 15 );
