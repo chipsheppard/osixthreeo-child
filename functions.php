4cd18bf4cb2e2c30135256970a8247b30260743e
Chip Sheppard <chip@sheppco.com>
2020-03-05 18:57:25 -0500

    remove enque-child-theme

--- functions.php
+++ functions.php
@@ -26,10 +26,6 @@ if ( ! function_exists( 'osixthreeo_child_enqueue_styles' ) ) {
 		// Enqueue Parent theme's stylesheet.
 		wp_enqueue_style( 'osixthreeo-parent-style', get_template_directory_uri() . '/style.css', array(), OSIXTHREEO_VERSION );
 
-		// Enqueue Child theme's stylesheet.
-		// Setting 'parent-style' as a dependency will ensure that the child theme stylesheet loads after it.
-		wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'osixthreeo-parent-style' ), OSIXTHREEO_CHILD_VERSION );
-
 	}
 }
 add_action( 'wp_enqueue_scripts', 'osixthreeo_child_enqueue_styles', 15 );
